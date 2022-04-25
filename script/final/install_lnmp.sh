61b59dda46a2c31f881d733aa6c60caaa1102f3e
wsxq2 <wsxq2@qq.com>
2022-04-25 19:51:58 +0800

    feat: add final

---
+++ script/final/install_lnmp.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+#
+# 简要说明
+#
+
+# 当命令返回非 0 或使用未设置变量时强行退出
+set -eu
+
+# 调试开关（显示执行的命令）
+set -x
+
+
+disable_selinux() {
+    if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        setenforce 0
+    fi
+}
+
+main(){
+    disable_selinux
+    systemctl stop firewalld
+    yum install wget git nginx mariadb mariadb-server php php-fpm php-pdo php-mysql -y
+    echo -ne '\n\nqwer\nqwer\n\n\n\n' | mysql_secure_installation
+    git clone https://github.com/wsxq2/BusSecurityManagement.git
+    cd BusSecurityManagement
+    cp -r front-end/web/* /usr/share/nginx/html/
+    sed -i 's/your mysql root password/qwer/g' back-end/all.sh
+
+    cat <<EOF > /etc/nginx/nginx.conf
+# For more information on configuration, see:
+#   * Official English Documentation: http://nginx.org/en/docs/
+#   * Official Russian Documentation: http://nginx.org/ru/docs/
+
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 4096;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen       80;
+        listen       [::]:80;
+        server_name  _;
+        root         /usr/share/nginx/html;
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        location / {
+            index index.php index.html index.htm;
+        }
+        #添加下列信息，配置Nginx通过fastcgi方式处理您的PHP请求。
+        location ~ .php$ {
+            root /usr/share/nginx/html;    #将/usr/share/nginx/html替换为您的网站根目录，本文使用/usr/share/nginx/html作为网站根目录。
+            fastcgi_pass 127.0.0.1:9000;   #Nginx通过本机的9000端口将PHP请求转发给PHP-FPM进行处理。
+            fastcgi_index index.php;
+            fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+            include fastcgi_params;   #Nginx调用fastcgi接口处理PHP请求。
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+        }
+    }
+
+# Settings for a TLS enabled server.
+#
+#    server {
+#        listen       443 ssl http2;
+#        listen       [::]:443 ssl http2;
+#        server_name  _;
+#        root         /usr/share/nginx/html;
+#
+#        ssl_certificate "/etc/pki/nginx/server.crt";
+#        ssl_certificate_key "/etc/pki/nginx/private/server.key";
+#        ssl_session_cache shared:SSL:1m;
+#        ssl_session_timeout  10m;
+#        ssl_ciphers HIGH:!aNULL:!MD5;
+#        ssl_prefer_server_ciphers on;
+#
+#        # Load configuration files for the default server block.
+#        include /etc/nginx/default.d/*.conf;
+#
+#        error_page 404 /404.html;
+#            location = /40x.html {
+#        }
+#
+#        error_page 500 502 503 504 /50x.html;
+#            location = /50x.html {
+#        }
+#    }
+
+}
+
+EOF
+
+    systemctl start mariadb
+    systemctl start nginx
+    systemctl start php-fpm
+
+    bash back-end/all.sh
+
+    systemctl enable nginx
+    systemctl enable mariadb
+    systemctl enable php-fpm
+}
+main
